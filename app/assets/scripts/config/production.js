5e216b7862938cb2e90b1e60ea5e37ff5ce82dd4
Nick Bumbarger <nbumbarger@gmail.com>
2017-07-12 16:16:41 -0400

    Add Mapbox accessToken to config

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -1,6 +1,7 @@
 'use strict';
 
 export default {
+  accessToken: 'pk.eyJ1IjoibmJ1bWJhcmciLCJhIjoiWG1NN1BlYyJ9.nbifRhdBcN1K-mdtwwi0eQ',
   environment: 'production',
   consoleMessage: 'production'
 };
