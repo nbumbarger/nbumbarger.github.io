d0281480cc0d200fb6d2b4771649bef4ea1e5a69
Nick Bumbarger <nbumbarger@gmail.com>
2017-07-13 18:15:19 -0400

    Add entry animation to header

--- app/assets/scripts/components/header.js
+++ app/assets/scripts/components/header.js
@@ -1,12 +1,41 @@
+'use strict'
+
 import React, { Component } from 'react'
+import { Motion, spring } from 'react-motion'
 
 class Header extends Component {
   render () {
     return (
       <header className='header'>
         <a href='mailto:nbumbarger@gmail.com'>
-          <h1>Nick B</h1>
-          <h2>Web and Geospatial Developer</h2>
+          <Motion defaultStyle={{x: -25, y: 0}} style={{
+            x: spring(0, {stiffness: 150, damping: 25}),
+            y: spring(1, {stiffness: 30, damping: 15})
+          }}>
+            {interpolation => {
+              return (
+                <div style={{
+                  marginLeft: `${interpolation.x}rem`,
+                  opacity: interpolation.y}}>
+                  <h1>Nick B</h1>
+                </div>
+              )
+            }}
+            </Motion>
+            <Motion defaultStyle={{x: -50, y: 0}} style={{
+              x: spring(0, {stiffness: 120, damping: 20}),
+              y: spring(1, {stiffness: 20, damping: 20})
+            }}>
+              {interpolation => {
+                return (
+                  <div style={{
+                    marginLeft: `${interpolation.x}rem`,
+                    opacity: interpolation.y}}>
+                    <h2>Web and Geospatial Developer</h2>
+                  </div>
+                )
+              }}
+            </Motion>
         </a>
       </header>
     )
