8c3e400fb44bcd5c0737108bc8b8af42da4f0dbe
Nick Bumbarger <nbumbarger@gmail.com>
2017-07-17 12:48:31 -0400

    Fix layout collapse bug

--- app/assets/scripts/components/header.js
+++ app/assets/scripts/components/header.js
@@ -11,7 +11,7 @@ class Header extends Component {
     super(props)
     this.state = {
       mobileWidth: false,
-      navHintActive: false,
+      navHintActive: true,
       navActive: false
     }
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
